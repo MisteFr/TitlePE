91b9f1c0c71e9e11dc16732d59ff9e3ad6d201a6
Misteboss <misteboss1@gmail.com>
2017-04-03 19:08:27 +0200

    Revert "Reorganization TitlePE by @MineBuilderFR"
    This reverts commit 84128d38651aeb412a29fe0b2ef341163a8979fe.

--- src/misteboss/Title.php
+++ src/misteboss/Title.php
@@ -2,30 +2,56 @@
 
 namespace misteboss;
 
-use misteboss\commands\TitleCommand;
-use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use misteboss\commands\TitleCommand;
+use pocketmine\command\Command;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\network\mcpe\protocol\SetTitlePacket;
 
-Class Title extends PluginBase implements Listener
-{
+Class Title extends PluginBase implements Listener{
 
-    CONST prefix = TextFormat::GOLD . TextFormat::BOLD . "TITLE" . TextFormat::YELLOW . " »" . TextFormat::RESET;
-    CONST author = "Misteboss MCPE";
+CONST prefix = "§6§lTITLE §e»§r ";
 
-    public function onEnable()
-    {
-        $this->getLogger()->info(self::prefix . "§aTitle Plugin has been activated !\n§7Developped by §c@" . self::author);
-        $this->RegisterListener();
-    }
-
-    /*
-     * Register All Event in Private Function . Why? I don't know <3
-     */
-    private function RegisterListener()
-    {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getServer()->getCommandMap()->register("titleplus", new TitleCommand($this, "titleplus"));
-    }
+public function onEnable(){
+$this->getLogger()->info(self::prefix."§aTitle Plugin has been activated !\n§7Developped by §c@Misteboss_mcpe");
+$this->getServer()->getCommandMap()->register("title", new TitleCommand($this, "title"));
 }
 
+public function addTitle($player, string $title, string $subtitle = "", int $fadeIn = -1, int $stay = -1, int $fadeOut = -1){
+$this->resetTitleSettings($player);
+$this->setTitleDuration($player, $fadeIn, $stay, $fadeOut);
+$this->sendTitleText($player,$title, SetTitlePacket::TYPE_SET_TITLE);
+$this->sendTitleText($player, $subtitle, SetTitlePacket::TYPE_SET_SUBTITLE);
+}
+
+public function resetTitleSettings(Player $player){
+$pk = new SetTitlePacket();
+$pk->type = SetTitlePacket::TYPE_RESET_TITLE;
+$player->dataPacket($pk);
+}
+
+public function setTitleDuration(Player $player, int $fadeIn, int $stay, int $fadeOut){
+if($fadeIn >= 0 and $stay >= 0 and $fadeOut >= 0){
+$pk = new SetTitlePacket();
+$pk->type = SetTitlePacket::TYPE_SET_ANIMATION_TIMES;
+$pk->fadeInTime = $fadeIn;
+$pk->stayTime = $stay;
+$pk->fadeOutTime = $fadeOut;
+$player->dataPacket($pk);
+}
+}
+  
+public function sendTitleText(Player $player,string $title, int $type){
+$pk = new SetTitlePacket();
+$pk->type = $type;
+$pk->text = $title;
+$player->dataPacket($pk);
+}
+  
+public function actionBar(Player $player, string $message){
+$this->sendTitleText($player, $message, SetTitlePacket::TYPE_SET_ACTIONBAR_MESSAGE);
+}
+  
+}
--- src/misteboss/commands/TitleCommand.php
+++ src/misteboss/commands/TitleCommand.php
@@ -2,64 +2,43 @@
 
 namespace misteboss\commands;
 
-use misteboss\API\API;
-use misteboss\Title;
-use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\command\PluginIdentifiableCommand;
-use pocketmine\Player;
+use pocketmine\command\PluginCommand;
+use misteboss\Title;
 use pocketmine\utils\TextFormat as TF;
+use pocketmine\Player;
 
-class TitleCommand extends Command implements PluginIdentifiableCommand
-{
+class TitleCommand extends PluginCommand {
 
-    public $usage = "/titleplus <player / @all> <title> <subtitle> <fadeIn (20 = 1sec)> <duration (20 = 1sec)> <fadeOut (20 = 1sec)>";
-    private $main;
-    private $name;
+private $pg;
 
-    public function __construct(Title $main, $name)
-    {
-        parent::__construct($name, $main);
-        $this->plugin = $main;
-        $this->setPermission("titleplus.cmd");
-        $this->setDescription("Send Title with command !");
-        $this->setUsage($this->usage);
-    }
+public function __construct(Title $main, $name) {
+parent::__construct($name, $main);
+$this->pg = $main;
+$this->setPermission("title.command");
+}
 
-    public function getPlugin()
-    {
-        return $this->plugin;
-    }
-
-    public function execute(CommandSender $sender, $currentAlias, array $args)
-    {
-        if (!$this->plugin->isEnabled()) return false;
-        if ($sender instanceof Player) {
-            if (count($args) < 2) {
-                $sender->sendMessage(Title::prefix . TF::GRAY . "Usage : " . $this->usage);
-            } else {
-                if (!$this->plugin->getServer()->getPlayer($args[0]) instanceof Player) {
-                    $sender->sendMessage(TF::DARK_RED . $args[0] . " is not online !");
-                    return;
-                }
-                if (!isset($args[2])) $args[2] = "";
-                if (!isset($args[3]) or !is_numeric($args[3])) $args[3] = 20;
-                if (!isset($args[4]) or !is_numeric($args[4])) $args[4] = 20;
-                if (!isset($args[5]) or !is_numeric($args[5])) $args[5] = 20;
-                if (strtolower($args[0]) == '@all' or strtolower($args[0]) == '@a') {
-                    foreach ($this->plugin->getServer()->getOnlinePlayers() as $players) {
-                        API::addTitle($players, $args[1], $args[2], $args[3], $args[4], $args[5]);
-                    }
-                    $sender->sendMessage(Title::prefix . TF::GREEN . ' Correctly send ' . $args[1] . ' ' . $args[2] . ' as title to all online players (fadeIn : ' . $args[3] . ') (duration : ' . $args[4] . ') (fadeOut : ' . $args[5] . ')');
-                } else {
-                    $player = $sender->getServer()->getPlayerExact($args[0]);
-                    API::addTitle($player, $args[1], $args[2], $args[3], $args[4], $args[5]);
-                    $sender->sendMessage(Title::prefix . TF::GREEN . ' Correctly send ' . $args[1] . ' ' . $args[2] . ' as title to ' . $player->getName() . '(fadeIn : ' . $args[3] . ') (duration : ' . $args[4] . ') (fadeOut : ' . $args[5] . ')');
-                }
-            }
-        } else {
-            $sender->sendMessage(Title::prefix . TF::RED . "You must run this command in game!");
-        }
-    }
+public function execute(CommandSender $sender, $currentAlias, array $args) {
+if($this->testPermission($sender)){
+if($sender instanceof Player){
+if(count($args) < 5){
+$sender->sendMessage(Title::prefix."§7Usage : /title <player / all> <title> <subtitle> <fadeIn (20 = 1sec)> <duration (20 = 1sec)> <fadeOut (20 = 1sec)>");
+}else{
+if($args[0] == 'all'){
+foreach($this->pg->getServer()->getOnlinePlayers() as $players){
+$this->pg->addTitle($players, $args[1], $args[2], $args[3], $args[4], $args[5]);
+}
+$sender->sendMessage(Title::prefix.' §aCorrectly send ' .$args[1]. ' ' .$args[2]. ' as title to all online players (fadeIn : '.$args[3]. ') (duration : '.$args[4]. ') (fadeOut : ' .$args[5]. ')');
+}else{
+$player = $sender->getServer()->getPlayerExact($args[0]);
+$this->pg->addTitle($player, $args[1], $args[2], $args[3], $args[4], $args[5]);
+$sender->sendMessage(Title::prefix.' §aCorrectly send ' .$args[1]. ' ' .$args[2]. ' as title to '.$player->getName().'(fadeIn : '.$args[3]. ') (duration : '.$args[4]. ') (fadeOut : ' .$args[5]. ')');
+}
+}
+}else{
+$sender->sendMessage(Title::prefix."§cYou must run this command in game!");
+}
+}
+}
 }
 
