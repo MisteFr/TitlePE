e06e7935d21bd2530ef0497041700f2df11ef2a1
MineBuilderFR <minebuilderfr11@gmail.com>
2017-04-03 23:51:42 +0200

    Update TitleCommand.php

--- src/misteboss/commands/TitleCommand.php
+++ src/misteboss/commands/TitleCommand.php
@@ -38,23 +38,23 @@ class TitleCommand extends Command implements PluginIdentifiableCommand
             if (count($args) < 2) {
                 $sender->sendMessage(Title::prefix . TF::GRAY . "Usage : " . $this->usage);
             } else {
-                if (!$this->plugin->getServer()->getPlayer($args[0]) instanceof Player) {
-                    $sender->sendMessage(TF::DARK_RED . $args[0] . " is not online !");
-                    return;
-                }
                 if (!isset($args[2])) $args[2] = "";
                 if (!isset($args[3]) or !is_numeric($args[3])) $args[3] = 20;
                 if (!isset($args[4]) or !is_numeric($args[4])) $args[4] = 20;
                 if (!isset($args[5]) or !is_numeric($args[5])) $args[5] = 20;
-                if (strtolower($args[0]) == '@all' or strtolower($args[0]) == '@a') {
+                if (strtolower($args[0]) == "@all" or strtolower($args[0]) == "@a") {
                     foreach ($this->plugin->getServer()->getOnlinePlayers() as $players) {
                         API::addTitle($players, $args[1], $args[2], $args[3], $args[4], $args[5]);
                     }
-                    $sender->sendMessage(Title::prefix . TF::GREEN . ' Correctly send ' . $args[1] . ' ' . $args[2] . ' as title to all online players (fadeIn : ' . $args[3] . ') (duration : ' . $args[4] . ') (fadeOut : ' . $args[5] . ')');
+                    $sender->sendMessage(Title::prefix . TF::GREEN . ' Correctly send " ' . $args[1] . ' " " ' . $args[2] . ' " as title to all online players (fadeIn : ' . $args[3] . ') (duration : ' . $args[4] . ') (fadeOut : ' . $args[5] . ')');
                 } else {
+                    if (!$this->plugin->getServer()->getPlayer($args[0]) instanceof Player) {
+                        $sender->sendMessage(TF::DARK_RED . $args[0] . " is not online !");
+                        return;
+                    }
                     $player = $sender->getServer()->getPlayerExact($args[0]);
                     API::addTitle($player, $args[1], $args[2], $args[3], $args[4], $args[5]);
-                    $sender->sendMessage(Title::prefix . TF::GREEN . ' Correctly send ' . $args[1] . ' ' . $args[2] . ' as title to ' . $player->getName() . '(fadeIn : ' . $args[3] . ') (duration : ' . $args[4] . ') (fadeOut : ' . $args[5] . ')');
+                    $sender->sendMessage(Title::prefix . TF::GREEN . ' Correctly send " ' . $args[1] . ' " " ' . $args[2] . ' " as title to ' . $player->getName() . '(fadeIn : ' . $args[3] . ') (duration : ' . $args[4] . ') (fadeOut : ' . $args[5] . ')');
                 }
             }
         } else {
@@ -62,4 +62,3 @@ class TitleCommand extends Command implements PluginIdentifiableCommand
         }
     }
 }
-
